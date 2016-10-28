5c08263b08a378aa03f78aeb0792e73a5c9532a9
Fernando Garcia <fkngv_10@hotmail.com>
2016-10-28 00:04:44 -1000

    Diseño Reporte de Productos
    Creación de formulario Reporte_Productos

--- Cafeteria/Cafeteria/Reporte_Productos.Designer.cs
+++ Cafeteria/Cafeteria/Reporte_Productos.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cafeteria
 {
-    partial class Consulta_Producto
+    partial class Reporte_Productos
     {
         /// <summary>
         /// Required designer variable.
@@ -28,26 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnRegresar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.lblProducto = new System.Windows.Forms.Label();
-            this.txtProducto = new System.Windows.Forms.TextBox();
-            this.btnBuscar = new System.Windows.Forms.Button();
-            this.dgvProducto = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvProducto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.BackgroundColor = System.Drawing.Color.Tan;
+            this.dgvProductos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(26, 27);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(656, 150);
+            this.dgvProductos.TabIndex = 0;
             // 
             // btnEliminar
             // 
             this.btnEliminar.BackColor = System.Drawing.Color.Brown;
             this.btnEliminar.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEliminar.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnEliminar.Location = new System.Drawing.Point(26, 213);
+            this.btnEliminar.Location = new System.Drawing.Point(26, 212);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(120, 36);
-            this.btnEliminar.TabIndex = 0;
+            this.btnEliminar.Size = new System.Drawing.Size(115, 42);
+            this.btnEliminar.TabIndex = 1;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = false;
             // 
@@ -56,10 +63,10 @@
             this.btnModificar.BackColor = System.Drawing.Color.Brown;
             this.btnModificar.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnModificar.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnModificar.Location = new System.Drawing.Point(152, 214);
+            this.btnModificar.Location = new System.Drawing.Point(147, 212);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(120, 36);
-            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Size = new System.Drawing.Size(115, 40);
+            this.btnModificar.TabIndex = 2;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = false;
             // 
@@ -68,10 +75,10 @@
             this.btnRegresar.BackColor = System.Drawing.Color.DarkRed;
             this.btnRegresar.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegresar.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnRegresar.Location = new System.Drawing.Point(426, 214);
+            this.btnRegresar.Location = new System.Drawing.Point(446, 213);
             this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(120, 36);
-            this.btnRegresar.TabIndex = 2;
+            this.btnRegresar.Size = new System.Drawing.Size(115, 40);
+            this.btnRegresar.TabIndex = 3;
             this.btnRegresar.Text = "Regresar";
             this.btnRegresar.UseVisualStyleBackColor = false;
             // 
@@ -80,83 +87,38 @@
             this.btnSalir.BackColor = System.Drawing.Color.DarkRed;
             this.btnSalir.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalir.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnSalir.Location = new System.Drawing.Point(552, 213);
+            this.btnSalir.Location = new System.Drawing.Point(567, 212);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(120, 36);
-            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Size = new System.Drawing.Size(115, 40);
+            this.btnSalir.TabIndex = 4;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = false;
             // 
-            // lblProducto
-            // 
-            this.lblProducto.AutoSize = true;
-            this.lblProducto.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProducto.Location = new System.Drawing.Point(136, 24);
-            this.lblProducto.Name = "lblProducto";
-            this.lblProducto.Size = new System.Drawing.Size(93, 21);
-            this.lblProducto.TabIndex = 4;
-            this.lblProducto.Text = "Producto:";
-            // 
-            // txtProducto
-            // 
-            this.txtProducto.Location = new System.Drawing.Point(235, 27);
-            this.txtProducto.Name = "txtProducto";
-            this.txtProducto.Size = new System.Drawing.Size(265, 20);
-            this.txtProducto.TabIndex = 5;
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.BackColor = System.Drawing.Color.IndianRed;
-            this.btnBuscar.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnBuscar.Location = new System.Drawing.Point(506, 20);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(81, 31);
-            this.btnBuscar.TabIndex = 6;
-            this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = false;
-            // 
-            // dgvProducto
-            // 
-            this.dgvProducto.BackgroundColor = System.Drawing.Color.Tan;
-            this.dgvProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProducto.Location = new System.Drawing.Point(26, 138);
-            this.dgvProducto.Name = "dgvProducto";
-            this.dgvProducto.Size = new System.Drawing.Size(646, 47);
-            this.dgvProducto.TabIndex = 7;
-            // 
-            // Consulta_Producto
+            // Reporte_Productos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Cafeteria.Properties.Resources.fondo;
-            this.ClientSize = new System.Drawing.Size(708, 265);
-            this.Controls.Add(this.dgvProducto);
-            this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.txtProducto);
-            this.Controls.Add(this.lblProducto);
+            this.ClientSize = new System.Drawing.Size(708, 403);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dgvProductos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "Consulta_Producto";
-            this.Text = "Consultar Producto";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvProducto)).EndInit();
+            this.Name = "Reporte_Productos";
+            this.Text = "Reporte Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dgvProductos;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnRegresar;
         private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.Label lblProducto;
-        private System.Windows.Forms.TextBox txtProducto;
-        private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.DataGridView dgvProducto;
     }
 }
