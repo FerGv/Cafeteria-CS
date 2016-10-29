fc15f7f17e9f0a6c006e2a2d701c8264e6854717
Fernando Garcia <fkngv_10@hotmail.com>
2016-10-28 23:57:10 -1000

    Funcionalidad Login
    Creación del inicio de sesión

--- Cafeteria/Cafeteria/Login.cs
+++ Cafeteria/Cafeteria/Login.cs
@@ -16,5 +16,32 @@ namespace Cafeteria
         {
             InitializeComponent();
         }
+
+        public Usuario user { get; set; }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "")
+                MessageBox.Show("Debes llenar el nombre");
+            else
+            {
+                if (txtPass.Text == "")
+                    MessageBox.Show("Debes llenar la contraseña");
+                else
+                {
+                    user = Funciones.Consulta_Usuario(txtNombre.Text, txtPass.Text);
+
+                    if ((user.usuario == txtNombre.Text) && (user.pass == txtPass.Text))
+                    {
+                        MessageBox.Show("Bienvenido");
+                        Menu menu = new Menu();
+                        menu.Show();
+                        Close();
+                    }
+                    else
+                        MessageBox.Show("Datos incorrectos");
+                }
+            }
+        }
     }
 }
