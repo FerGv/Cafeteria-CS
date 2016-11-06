fdde1b9c30573f70a43616f0a0ef0beb308f36e8
Fernando Garcia <fkngv_10@hotmail.com>
2016-11-06 10:22:47 -0600

    Indentación

--- BD y tablas.sql
+++ BD y tablas.sql
@@ -3,19 +3,19 @@ use cafeteria;
 
 create table usuarios (
 	id_usuario	INT NOT NULL AUTO_INCREMENT,
-    usuario 	VARCHAR(50) NOT NULL,
+	usuario 	VARCHAR(50) NOT NULL,
 	pass 		VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id_usuario)
+	PRIMARY KEY(id_usuario)
 ) ENGINE=InnoDB;
 
 create table materia_prima (
 	id_materia	INT NOT NULL AUTO_INCREMENT,
-    producto 	VARCHAR(50) NOT NULL,
-    cantidad 	FLOAT NOT NULL,
+	producto 	VARCHAR(50) NOT NULL,
+	cantidad 	FLOAT NOT NULL,
 	unidad 		VARCHAR(10) NOT NULL,
-    precio 		FLOAT NOT NULL,
-    stock 		FLOAT NOT NULL,
-    PRIMARY KEY(id_materia)
+	precio 		FLOAT NOT NULL,
+	stock 		FLOAT NOT NULL,
+	PRIMARY KEY(id_materia)
 ) ENGINE=InnoDB;
 
 INSERT INTO usuarios (usuario, pass) VALUES ('admin', '123');
